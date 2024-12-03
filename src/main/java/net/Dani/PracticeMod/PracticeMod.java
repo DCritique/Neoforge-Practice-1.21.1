c9e3c1e83d0bc6ddcac9d8458efece83a6eafe4b
Danyr <danyrenteria12345@gmail.com>
2024-12-03 15:48:39 -0500

    Changes some things

--- src/main/java/net/Dani/PracticeMod/PracticeMod.java
+++ src/main/java/net/Dani/PracticeMod/PracticeMod.java
@@ -1,5 +1,7 @@
 package net.Dani.PracticeMod;
 
+import net.Dani.PracticeMod.block.ModBlocks;
+import net.Dani.PracticeMod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -57,6 +59,8 @@ public class PracticeMod
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -73,7 +77,14 @@ public class PracticeMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS) {
+        event.accept(ModItems.BISMUTH);
+        event.accept(ModItems.RAW_BISMUTH);
+    }
+        if(event.getTabKey()== CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
